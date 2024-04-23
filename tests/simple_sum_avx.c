0caf05f631f429ea5cd5dcdc3713a7e606b4da12
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2024-04-23 14:48:28 -0300

    Expand test AVX calculation for 2048 window

--- tests/simple_sum_avx.c
+++ tests/simple_sum_avx.c
@@ -17,23 +17,28 @@ void fillVector(int *vector, int size, int value) {
 
 
 int main(int argc, char const *argv[]) {
-    int* data_a = (int*) aligned_alloc(32, 8 * sizeof(int));
-    int* data_b = (int*) aligned_alloc(32, 8 * sizeof(int));
-    int* data_sum = (int*) aligned_alloc(32, 8 * sizeof(int));
+    int* data_a = (int*) aligned_alloc(32, 2048 * sizeof(int));
+    int* data_b = (int*) aligned_alloc(32, 2048 * sizeof(int));
+    int* data_sum = (int*) aligned_alloc(32, 2048 * sizeof(int));
 
-    fillVector(data_a, 8, 10);
-    fillVector(data_b, 8, 20);
+    //int numSteps = 2048 / 8;
+    fillVector(data_a, 2048, 10);
+    fillVector(data_b, 2048, 20);
 
-    __m256i vec_a, vec_b, vec_sum;
+    for (int i = 0; i < 2048; i+=8) {
+        int* pointer_a = data_a + i;
+        int* pointer_b = data_b + i;
+        int* pointer_sum = data_sum + i;
 
-    printf("AAAA\n");
+        __m256i vec_a, vec_b, vec_sum;
 
-    vec_a = _mm256_load_si256((__m256i *)data_a);
-    vec_b = _mm256_load_si256((__m256i *)data_b);
+        vec_a = _mm256_load_si256((__m256i *)pointer_a);
+        vec_b = _mm256_load_si256((__m256i *)pointer_b);
 
-    vec_sum = _mm256_add_epi32(vec_a, vec_b);
-    
-    _mm256_store_si256((__m256i *) data_sum, vec_sum);
+        vec_sum = _mm256_add_epi32(vec_a, vec_b);
+        
+        _mm256_store_si256((__m256i *) pointer_sum, vec_sum);
+    }
 
-    imprime(data_sum, 8);
+    imprime(data_sum, 2048);
 }
